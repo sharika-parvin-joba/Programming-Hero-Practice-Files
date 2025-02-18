b33b8464b8fb9a2536764b6a114a0b97aaffc2b2
unknown <sharikajoba@gmail.com>
2025-02-18 16:54:08 +0600

    commit

---
+++ proghero/p4-(common js concepts)/equal.js
@@ -0,0 +1,14 @@
+//=== triple equal value and type 2tai COMPARE kore BUT == double equal value COMPARE kore not type
+//BETTER TO USE TRIPLE EQUAL
+const a=2;
+const b=2
+const c="2";
+
+if(a==c){
+    console.log('equal')
+}else{console.log('not equal')}
+
+if(b===c){
+    console.log('equal1')
+}else{console.log('not equal1')
+}
