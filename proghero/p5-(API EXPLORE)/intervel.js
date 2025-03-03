97f63c54f822a75ac440d65d099abcc4f1313183
unknown <sharikajoba@gmail.com>
2025-03-03 12:52:01 +0600

    commit

---
+++ proghero/p5-(API EXPLORE)/intervel.js
@@ -0,0 +1,13 @@
+console.log(1);
+console.log(2);
+//SETTIMEOUT
+setTimeout(()=>{
+    console.log(3);
+},1000)
+
+console.log(4);
+console.log(5);
+//SETINTERVAL- choltey thakbe jodi jor kore off na kore 
+setInterval(()=>{
+    console.log(6);
+},2000)
